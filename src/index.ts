154665561b63f318a070b7271cdb64f601b34554
Arlan Jaska <ajaska@berkeley.edu>
2019-12-02 13:37:05 -0800

    Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 interface SomeInterface {
-  thing: number
+  thing: number;
 }
 
-let x: SomeInterface = {thing : 7}
-console.log(x)
-console.log(x.thing + 1)
+let x: SomeInterface = { thing: 7 };
+console.log(x);
+console.log(x.thing + 1);
